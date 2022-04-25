14a96cb610985507f0d2e883c5db8cce99aa0293
Chaos76 <104395443+Chaos76@users.noreply.github.com>
2022-04-25 15:44:46 -0600

    Update config.js

--- config.js
+++ config.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['916909137213','6283839980451'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['916909137213'] //another owner number
-global.premium = ['916909137213'] //premium number
-global.pengguna = 'Xeon' //username
-global.botnma = 'Cheems Bot-MD' //bot name
-global.ownernma = '🦄Dream Guy Xeon' //owner name
-global.packname = 'Cheems Bot' //sticker package name
-global.author = 'By Dream Guy Xeon' //sticker author name
+global.owner = ['+50258090849','50245369173'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['+50258090849'] //another owner number
+global.premium = ['+50245369173'] //premium number
+global.pengguna = 'Black' //username
+global.botnma = 'Blackcore Bot' //bot name
+global.ownernma = '🦄Blackcore' //owner name
+global.packname = 'Black Bot' //sticker package name
+global.author = 'By Blackcore bot☆' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '⭔' //design
